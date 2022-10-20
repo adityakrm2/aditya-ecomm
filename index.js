a2386f0e20c2d0877e1b19c09ecd5f15de4a7e54
Aditya Kumar <aditya.kr.m2@gmail.com>
2022-10-21 04:32:24 +0530

    make it bette22s2r

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.json())
 // app.get("/", (req, res) => {
 //     res.json("server started")
 // })
-app.use("/", slash);
+app.use("/api", slash);
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
